feab83bb76fadd5e6f33f382b33de34026abfc62
pernsteinerc <pc@pcode.at>
2024-12-22 18:41:46 +0100

    feat: add vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  // Must be 'static' or 'hybrid'
+  output: "static",
+  adapter: vercelStatic(),
 });
